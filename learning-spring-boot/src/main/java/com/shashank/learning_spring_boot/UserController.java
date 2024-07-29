861ab8940e4b5af57d80d109cbcbc214e96e9b78
Shashank Chauhan <shashanktech2@gmail.com>
2024-07-29 09:53:53 +0530

    Create first Rest Controller

---
+++ learning-spring-boot/src/main/java/com/shashank/learning_spring_boot/UserController.java
@@ -0,0 +1,23 @@
+package com.shashank.learning_spring_boot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+record User(int id,String name,int age){};
+
+@RestController
+public class UserController {
+
+    @RequestMapping("users")
+    public List<User> getUsers() {
+        List<User> userList=new ArrayList<>();
+        userList.add(new User(1,"Shashank",25));
+        userList.add(new User(2,"Elon",40));
+        userList.add(new User(3,"Amber",39));
+
+        return userList;
+    }
+}
