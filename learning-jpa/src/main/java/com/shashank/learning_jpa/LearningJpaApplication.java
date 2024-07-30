f6412ec4265c0975a2b219242f731d584fe5ca3c
Shashank Chauhan <shashanktech2@gmail.com>
2024-07-30 09:48:09 +0530

    Enabled H2 Console

---
+++ learning-jpa/src/main/java/com/shashank/learning_jpa/LearningJpaApplication.java
@@ -0,0 +1,13 @@
+package com.shashank.learning_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJpaApplication.class, args);
+	}
+
+}
---
+++ learning-jpa/src/test/java/com/shashank/learning_jpa/LearningJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashank.learning_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
