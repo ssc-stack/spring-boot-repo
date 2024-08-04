372eeec05e75bbaad2d613e6d084e2cb1a6a39db
Shashank Chauhan <shashanktech2@gmail.com>
2024-08-04 21:34:36 +0530

    Modified Default Spring Security Chain Filter

---
+++ social-media-app/src/main/java/com/shashank/social_media_app/security/SpringSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.shashank.social_media_app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //		1) All requests should be authenticated
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+//		2) If a request is not authenticated, a web page is shown
+        http.httpBasic(withDefaults());
+
+//		3) CSRF -> POST, PUT
+        // http.csrf().disable(); // Deprecated in SB 3.1.x
+        http.csrf(csrf -> csrf.disable()); // Starting from SB 3.1.x Using Lambda DSL
+        // OR
+        // http.csrf(AbstractHttpConfigurer::disable); // Starting from SB 3.1.x Using Method Reference
+
+
+        return http.build();
+    }
+}
