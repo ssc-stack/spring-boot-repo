ad13e1235c0474fa4f312dd491e1a4e4e5975430
Shashank Chauhan <shashanktech2@gmail.com>
2024-08-04 19:39:47 +0530

    Connected the App to H2 In-memory db

--- social-media-app/src/main/java/com/shashank/social_media_app/entity/Post.java
+++ social-media-app/src/main/java/com/shashank/social_media_app/entity/Post.java
@@ -1,4 +1,10 @@
 package com.shashank.social_media_app.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+//@Entity
 public class Post {
+    @Id
+    private int id;
 }
--- social-media-app/src/main/java/com/shashank/social_media_app/entity/User.java
+++ social-media-app/src/main/java/com/shashank/social_media_app/entity/User.java
@@ -1,15 +1,21 @@
 package com.shashank.social_media_app.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
+@Entity(name="user_details")
 public class User {
+    @Id
     private int id;
 
     @Size(min=2, message = "Username should be of minimum length 2")
