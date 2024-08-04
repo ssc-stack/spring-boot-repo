95e24e5f89652b55e6ec32661d34f3d9764a56e9
Shashank Chauhan <shashanktech2@gmail.com>
2024-08-04 10:59:43 +0530

    Api for finding a specific user by ID

--- social-media-app/src/main/java/com/shashank/social_media_app/controller/UserController.java
+++ social-media-app/src/main/java/com/shashank/social_media_app/controller/UserController.java
@@ -4,11 +4,14 @@ import com.shashank.social_media_app.entity.User;
 import com.shashank.social_media_app.service.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("users")
 public class UserController {
 
 
@@ -18,8 +21,13 @@ public class UserController {
         this.userDaoService=userDaoService;
     }
 
-    @GetMapping(path="users/find-all")
+    @GetMapping
     public List<User> getUsers() {
         return userDaoService.findAll();
+    }
+
+    @GetMapping(path="/id/{id}")
+    public User getUserById(@PathVariable int id) {
+        return userDaoService.findOne(id);
     }
 }
