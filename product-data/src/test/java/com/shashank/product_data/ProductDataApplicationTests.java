969e47b5293b856d0e317db9cf63e1971dc23b93
sscstack <shashanktech2@gmail.com>
2024-08-31 16:50:36 +0530

    Creating Spring Data Project

---
+++ product-data/src/test/java/com/shashank/product_data/ProductDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashank.product_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
